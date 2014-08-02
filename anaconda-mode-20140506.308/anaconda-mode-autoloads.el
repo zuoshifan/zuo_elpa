3145b7febe0f429fa18c282b4e62cf27d25ded23
zuoshifan <zuoshifan@163.com>
2014-08-02 21:01:15 +0800

    Update anaconda-mode.

--- anaconda-mode-20140506.308/anaconda-mode-autoloads.el
+++
@@ -1,54 +0,0 @@
-;;; anaconda-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (anaconda-eldoc) "anaconda-eldoc" "anaconda-eldoc.el"
-;;;;;;  (21365 49639 414252 573000))
-;;; Generated autoloads from anaconda-eldoc.el
-
-(autoload 'anaconda-eldoc "anaconda-eldoc" "\
-ElDoc for anaconda-mode.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads (anaconda-mode) "anaconda-mode" "anaconda-mode.el"
-;;;;;;  (21365 49639 370252 367000))
-;;; Generated autoloads from anaconda-mode.el
-
-(autoload 'anaconda-mode "anaconda-mode" "\
-Code navigation, documentation lookup and completion for Python.
-
-\\{anaconda-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads (company-anaconda) "company-anaconda" "company-anaconda.el"
-;;;;;;  (21365 49639 178251 409000))
-;;; Generated autoloads from company-anaconda.el
-
-(autoload 'company-anaconda "company-anaconda" "\
-Jedi backend for company-mode.
-See `company-backends' for more info about COMMAND and ARG.
-
-\(fn COMMAND &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("anaconda-mode-pkg.el") (21365 49641 420519
-;;;;;;  776000))
-
-;;;***
-
-(provide 'anaconda-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; anaconda-mode-autoloads.el ends here
